8c49a624fae0b2192ee93713bbb6c40688aabe71
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-06-15 17:16:30 +0300

    adding ISensor-interface template and IAgent-interface template

--- InvestigationGameApp/Controllers/GameController.cs
+++ InvestigationGameApp/Controllers/GameController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using InvestigationGameApp.Models;
+using InvestigationGameApp.Models.Interfaces;
+using InvestigationGameApp.Models.Sensors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,26 @@ namespace InvestigationGameApp.Controllers
 {
     internal class GameController
     {
+        private InvestigationRoom room;
+
+        public void StartGame() { }
+        public void GameLoop() { }
+        public ISensor? CreateSensor(string type)
+        {
+            ISensor sensor = null;
+            if (type == "audio")
+            {
+                
+            }
+            else if (type == "thermal")
+            {
+
+            }
+            return sensor;
+        }
+        public void ShowStatus()
+        {
+
+        }
     }
 }
